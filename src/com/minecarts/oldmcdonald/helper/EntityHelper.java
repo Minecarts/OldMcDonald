9c4cecade0caa51a9e15cb58ec1c5bf2b3214316
Stephen <stephen@jazer.com>
2011-12-15 00:49:29 -0500

    Fix an error with null locations

--- src/com/minecarts/oldmcdonald/helper/EntityHelper.java
+++ src/com/minecarts/oldmcdonald/helper/EntityHelper.java
@@ -8,13 +8,6 @@ import org.bukkit.entity.Entity;
 
 import java.util.HashMap;
 
-/**
- * Created by IntelliJ IDEA.
- * User: stephen
- * Date: 12/13/11
- * Time: 6:53 PM
- * To change this template use File | Settings | File Templates.
- */
 public class EntityHelper {
     public static int getTotalEntities(Class type, World world){
         int count = 0;
@@ -28,7 +21,9 @@ public class EntityHelper {
     public static int countNearbyEntities(Class type, Location loc, int distance){
         int count = 0;
         for(Entity e : loc.getWorld().getEntities()){
-            if(type.isAssignableFrom(e.getClass()) && e.getLocation().distance(loc) <= distance){
+            Location entityLocation = e.getLocation();
+            if(entityLocation == null || loc == null || !entityLocation.getWorld().equals(loc.getWorld())) continue;
+            if(type.isAssignableFrom(e.getClass()) && entityLocation.distance(loc) <= distance){
                 count++;
             }
         }
